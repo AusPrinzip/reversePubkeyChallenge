c3290956bfa913c3a4ee7329cd7a6e92f1bb65a7
James Brown <cdmdb@hotmail.com>
2019-08-20 23:35:24 +0200

    updated dependencies

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const dsteem         = require('dsteem')
 const client         = new dsteem.client('https://api.steemit.com')
-const utils          = dsteem.cryptoUtils
+const cryptoUtils    = dsteem.cryptoUtils
 const utils          = require('./utils.js')
 const steemtrxfinder = require('steemtrxfinder')
 
 const test_post_url  = 'https://steemit.com/spt/@zaku/highlights-of-the-day-dq-and-mystery-rewards-neoxian-guild-tournament-result-neoxag-statics-and-spt-battle-gg-stake-curations'
 
 
-function reversePubkeyChallenge () {
+async function reversePubkeyChallenge () {
 	var author   = test_post_url.substring(test_post_url.lastIndexOf('@') + 1, test_post_url.lastIndexOf('/'))
 	var permlink = test_post_url.substr(test_post_url.lastIndexOf('/') + 1)
 
 	var content = await client.database.call('get_content', [author, permlink])
 	var active_votes = content[0].active_votes
 
-	active_votes.forEach((vote) => {
+	active_votes.forEach(async (vote) => {
 		let pseudo_trx
 		try {
 			pseudo_trx = await findVotePseudoTrx(client, vote)
@@ -29,7 +29,7 @@ function reversePubkeyChallenge () {
 			console.log(e)
 			return reject(vote)
 		}
-		let digest = utils.transactionDigest(trx)
+		let digest = cryptoUtils.transactionDigest(trx)
 
 		let signature
 		let pub = ''
@@ -46,3 +46,4 @@ function reversePubkeyChallenge () {
 	})
 }
  
+reversePubkeyChallenge()
